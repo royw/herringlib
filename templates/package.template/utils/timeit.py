47755af2588dc6d671157a4c1de4099e647740a4
royw <roy@wright.org>
2014-05-01 18:41:17 -0500

    added split_all and timeit to project utils

---
+++ templates/package.template/utils/timeit.py
@@ -0,0 +1,22 @@
+import tempfile
+import time
+
+timeit_enabled = False
+timeit_file = tempfile.NamedTemporaryFile(prefix="new-pcap-checkin", suffix="timeit", delete=False)
+
+
+def timeit(func):
+    """function timing decorator"""
+    def wrapper(*args, **kwargs):
+        if timeit_enabled:
+            ts = time.time()
+            result = func(*args, **kwargs)
+            te = time.time()
+            # timeit_file.file.write('func:%r args:[%r, %r] took: %2.4f sec\n' % (func.__name__, args, kwargs, te-ts))
+            timeit_file.file.write('func:%r took: %2.4f sec\n' % (func.__name__, te - ts))
+            return result
+        else:
+            return func(*args, **kwargs)
+    return wrapper
+
+
