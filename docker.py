deb36a808b6c84c7af98d5e5b7eb954da45f424c
royw <roy@wright.org>
2015-09-02 14:56:21 -0500

    added docker::new_image

--- docker.py
+++ docker.py
@@ -3,17 +3,45 @@
 Docker tasks
 """
 import os
+import shutil
+
 from herring.herring_app import namespace, task
 from herringlib.cd import cd
 from herringlib.local_shell import LocalShell
+from herringlib.mkdir_p import mkdir_p
 from herringlib.project_settings import Project
+from herringlib.prompt import prompt
 from herringlib.simple_logger import info
 from herringlib.split_all import split_all
+from herringlib.touch import touch
 
 DOCKERFILE = 'Dockerfile'
 
+DOCKERFILE_TEMPLATE = os.path.join(os.path.dirname(__file__), 'dockerfile.template')
+
 with namespace('docker'):
-    @task(depends=['build::containers', 'build::apps'])
+    @task(arg_prompt="Enter the image's name:")
+    def new_image():
+        """Create a new image directory and populate with a default Dockerfile."""
+        names = list(task.argv)
+        if not names:
+            if Project.prompt and task.arg_prompt is not None:
+                name = prompt(task.arg_prompt)
+                if name is not None and name.strip():
+                    names.append(name)
+
+        for name in names:
+            container_dir = os.path.join(Project.docker_dir, Project.docker_containers_dir, name)
+            mkdir_p(container_dir)
+            # populate container dir with Dockerfile and .dockerignore
+            dockerfile = os.path.join(container_dir, 'Dockerfile')
+            dockerignore = os.path.join(container_dir, '.dockerignore')
+            shutil.copyfile(DOCKERFILE_TEMPLATE, dockerfile)
+            touch(dockerignore)
+            info("Created folder for new image at: {dir}".format(dir=container_dir))
+
+
+    @task(depends=['build::containers'])
     def build():
         """
         Build docker images.
@@ -43,9 +71,3 @@ with namespace('docker'):
                     else:
                         info("skipping {dir}".format(dir=repo_dir))
 
-        @task()
-        def apps():
-            """
-            Build docker applications.
-            """
-            pass
