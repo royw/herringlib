adc72ec34b2af7db67490139eacf67bdea290bcb
royw <roy@wright.org>
2015-11-05 10:38:34 -0600

    added behave task

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ Add the following to your *requirements.txt* file:
 * coverage; python_version == "[test_python_versions]"
 * pytest; python_version == "[test_python_versions]"
 * decorator; python_version == "[test_python_versions]"
+* lettuce; python_version == "[test_python_versions]"
 
 """
 # noinspection PyUnresolvedReferences
@@ -42,3 +43,24 @@ if packages_required(required_packages):
             with LocalShell() as local:
                 info('Running unit tests using the current python environment')
                 local.run("py.test {tests_dir}".format(tests_dir=Project.tests_dir), verbose=True)
+
+
+    @task()
+    @venv_decorator(attr_name='test_python_versions')
+    def behave():
+        """Run the behavior tests."""
+
+        # Run the features in each of the virtual environments defined in Project.test_python_versions
+        # or if not defined, then in Project.wheel_python_versions.  If neither are defined, then
+        # run the test in the current environment.
+
+        venvs = VirtualenvInfo('test_python_versions', 'wheel_python_versions')
+
+        if not venvs.in_virtualenv and venvs.defined:
+            for venv_info in venvs.infos():
+                info('Running features using the {venv} virtual environment.'.format(venv=venv_info.venv))
+                venv_info.run('behave {features_dir}'.format(features_dir=Project.features_dir), verbose=True)
+        else:
+            with LocalShell() as local:
+                info('Running features using the current python environment')
+                local.run("behave {features_dir}".format(features_dir=Project.features_dir), verbose=True)
